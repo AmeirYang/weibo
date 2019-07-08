88d5573844105f1de9d01aed2b32eaeef377a113
root <1351617602@qq.com>
2019-07-08 17:59:06 +0800

    初始化样式

--- resources/views/StaticPages/home.blade.php
+++ resources/views/StaticPages/home.blade.php
@@ -1,8 +1,16 @@
 @extends('layouts.default')
 <!--加入layouts.default页面中的代码片段-->
-@section('title')
-        主页
-@endsection
 @section('content')
-    <h1>代码片段-主页</h1>
+    <div class="jumbotron">
+        <h1>AmeirYang博客</h1>
+        <p class="lead">
+            这是一个 博客 案例 。 
+        </p>
+        <p>
+          一切，将从这里开始。
+        </p>
+        <p>
+          <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @endsection
